e756df94c0ee2fe20dee8bdef1c9e1246f215460
Eisen Montalvo <eisen@mac.com>
2022-04-03 15:10:47 -0600

    Initial Commit

---
+++ Final Project/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Final Project
+//
+//  Created by Eisen Montalvo on 4/3/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
